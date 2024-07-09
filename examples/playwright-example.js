ad082a2b4dd0763b7420bb2fe4d7136d939be353
lyashenko.ivan <ivan.lyashenko.it@gmail.com>
2024-07-09 19:31:05 +0300

    improve "parser"

--- examples/playwright-example.js
+++ examples/playwright-example.js
@@ -1,26 +1,61 @@
 import {chromium} from 'playwright';
+import fs from 'fs';
 
-const url = 'https://playwright.dev';
-const selector = '.hero';
+const url = 'https://news.ycombinator.com/';
+const selector = '.athing';
+
+const parse = async (element) => {
+    const title = await element.$('.title > span > a');
+
+    return {
+        title: await title?.textContent(),
+        url: await title?.getAttribute('href'),
+    };
+};
+
+const writeDataToFile = (data) => {
+    try {
+        fs.writeFileSync('data.json', JSON.stringify(data));
+    } catch (error) {
+        console.error(error);
+    }
+};
 
 (async () => {
     const browser = await chromium.launch();
     const page = await browser.newPage();
-    await page.goto(url);
-    await page.click('.toggle_vylO');
-    await page.screenshot({path: 'example.png', fullPage: true});
-    await page.waitForTimeout(1000);
+    try {
+        await page.route(/(png|jpeg|jpg|gif|svg|webp|ico)$/, route => route.abort());
+        await page.goto(url);
+        await page.waitForTimeout(1000);
 
-    const result = await page.$$eval(selector, (elements) => {
-        return elements.map(element => {
-            const title = element.querySelector('.container > .hero__title')?.textContent || 'No title';
-            const gitHubStars = element.querySelector('.gh-count')?.textContent || 'No stars';
+        await page.evaluate(async () => {
+            await new Promise(resolve => {
+                let totalHeight = 0;
+                const distance = 100;
+                const timer = setInterval(() => {
+                    const scrollHeight = document.body.scrollHeight;
+                    window.scrollBy(0, distance);
+                    totalHeight += distance;
 
-            return {title, gitHubStars};
+                    if (totalHeight >= scrollHeight) {
+                        clearInterval(timer);
+                        resolve();
+                    }
+                }, 100);
+            });
         });
-    });
 
-    console.log(result);
+        const elements = await page.$$(selector);
 
-    await browser.close();
+        const result = await Promise.all(elements.map(element => parse(element)));
+
+        await writeDataToFile(result);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await page.screenshot({path: 'example.png', fullPage: true});
+        await page.close();
+        await browser.close();
+    }
 })();
